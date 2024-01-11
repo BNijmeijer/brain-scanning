f10971ec2d89c90383f965dd24ec352b260453db
Joris <joris.l@hotmail.com>
2024-01-11 15:06:17 +0100

    hoi

--- main.py
+++ main.py
@@ -1,35 +1,113 @@
-import os, time
-from tqdm import tqdm
-import h5py as h
+import h5py
 import numpy as np
+from scipy.stats import zscore
+import os
 import tensorflow as tf
-
-def get_dataset_name(path_with_dir : str):
-    dataset_name = path_with_dir.split('/')[-1]
-    dataset_name = '_'.join(dataset_name.split('_')[:-1])   # Removes final identifier number
-    return dataset_name
-
-def extract_data(data_folder : str):
-    print(f'Extracting data from {data_folder}...')
-    full_data = []
-    for file in tqdm(os.listdir(data_folder)):
-        full_path = '/'.join([data_folder, file])
-        with h.File(full_path,'r') as f:
-            dataset_name = get_dataset_name(full_path)
-            data_matrix = f.get(dataset_name)[()]
-            full_data.append(data_matrix)
-    print(np.shape(full_data))
-    return full_data
+import matplotlib.pyplot  as plt
+import preprocessing as p
 
 
-def main():
-    print('hi i guess')
-    cross_folder = './data/Cross/'
-    intra_folder = './data/Intra/'
-    train_data_cross = extract_data(os.path.join(cross_folder,'train'))
-    train_data_intra = extract_data(os.path.join(intra_folder,'train'))
+#y_train, x_train, y_test, x_test = p.load_intra()
 
-    
+y_train, x_train = p.load_cross_train()
+y_test, x_test = p.load_cross_test("1")
 
-if __name__ == '__main__':
-    main()
+
+x_train = p.preprocess(x_train, 5)
+x_test = p.preprocess(x_test, 5)
+
+y_train = p.relabel_all(y_train)
+y_test = p.relabel_all(y_test)
+print(y_test)
+
+#y_train = np.array([0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3])
+#y_test = np.array([0,0,1,1,2,2,3,3])
+
+# Variables
+INPUT_SHAPE = (248, 7125, 1)
+FILTER1_SIZE = 16
+FILTER2_SIZE = 64
+FILTER_SHAPE = (3, 3)
+POOL_SHAPE = (2, 25)
+FULLY_CONNECT_NUM = 64
+NUM_CLASSES = 4
+
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(FILTER1_SIZE, FILTER_SHAPE, activation='relu', input_shape=INPUT_SHAPE))
+model.add(tf.keras.layers.MaxPooling2D(POOL_SHAPE))
+model.add(tf.keras.layers.Conv2D(FILTER2_SIZE, FILTER_SHAPE, activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(POOL_SHAPE))
+model.add(tf.keras.layers.Conv2D(FILTER1_SIZE, FILTER_SHAPE, activation='relu'))
+# model.add(tf.keras.layers.LSTM(32, return_sequences=True))
+
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(FULLY_CONNECT_NUM, activation='relu'))
+model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
+
+model.summary()
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=5, 
+                    validation_data=(x_test, y_test))
+
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+#plt.show()
+# test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+
+
+
+#test_acc
+
+# mat = get_matrix(filename_path)
+# mat2 = downsample_matrix(mat, 5)
+# mat3 = zscale(mat2)
+# print(mat2.shape)
+# print(mat.shape)
+# print(mat3[:,0])
+
+
+
+
+
+
+# model = tf.keras.models.Sequential()
+
+# # Convolutional layers
+# model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(248, 7125, 1)))
+# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+# model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+# model.add(tf.keras.layers.MaxPooling2D((4, 4)))
+# model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu'))
+# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+
+# # Flatten layer
+# model.add(tf.keras.layers.Flatten())
+
+# # Dense layers
+# model.add(tf.keras.layers.Dense(64, activation='relu'))
+# # model.add(tf.keras.layers.Dense(16, activation='relu'))
+
+# # Output layer
+# model.add(tf.keras.layers.Dense(4, activation='softmax'))
+
+# # Compile the model
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+
+# # Display the model summary
+# model.summary()
+
+# history = model.fit(x_train, y_train, epochs=5, 
+#                     validation_data=(x_test, y_test))
---
+++ preprocessing.py
@@ -0,0 +1,72 @@
+import h5py
+import numpy as np
+from scipy.stats import zscore
+import os
+import tensorflow as tf
+import matplotlib.pyplot  as plt
+
+
+
+def get_dataset_name(filename_with_dir):
+    filename_without_dir = filename_with_dir.split('/')[-1]
+    temp = filename_without_dir.split('_')[:-1]
+    dataset_name = "_".join(temp)
+    return dataset_name
+
+#filename_path = "Intra/train/rest_105923_1.h5"
+# with h5py.File(filename_path, "r") as f:
+#     dataset_name = get_dataset_name(filename_path)
+#     matrix = f.get(dataset_name)[()]
+#     print(type(matrix))
+#     print(matrix.shape)
+
+def get_matrix(filename):
+    with h5py.File(filename, "r") as f:
+        dataset_name = get_dataset_name(filename)
+        matrix = f.get(dataset_name)[()]
+        return matrix
+
+def downsample_matrix(matrix, factor):
+    result_matrix = matrix[:,np.arange(0, matrix.shape[1], factor)]
+    return result_matrix
+
+def load_intra():
+    filenames = os.listdir("Intra/train")
+    x_train = [get_matrix("Intra/train/" + name) for name in filenames]
+    y_train = ['_'.join(name.split('_')[:-2]) for name in filenames]
+
+    filenames = os.listdir("Intra/test")
+    x_test = [get_matrix("Intra/test/" + name) for name in filenames]
+    y_test = ['_'.join(name.split('_')[:-2]) for name in filenames]
+    return y_train, x_train, y_test, x_test
+
+def load_cross_train():
+    filenames = os.listdir("Cross/train")
+    x_train = [get_matrix("Cross/train/" + name) for name in filenames]
+    y_train = ['_'.join(name.split('_')[:-2]) for name in filenames]
+    return y_train, x_train
+
+def load_cross_test(nr):
+    filenames = os.listdir("Cross/test"+nr)
+    x_train = [get_matrix("Cross/test"+nr+"/" + name) for name in filenames]
+    y_train = ['_'.join(name.split('_')[:-2]) for name in filenames]
+    return y_train, x_train
+
+def preprocess(matrices, factor):
+    mats = [downsample_matrix(matrix, factor) for matrix in matrices]
+    zs_mat = [zscore(matrix) for matrix in mats]
+    return np.array(zs_mat)
+
+def relabel(s):
+    if s.startswith("rest"):
+        return 0
+    elif s.startswith("task_motor"):
+        return 1
+    elif s.startswith("task_story"):
+        return 2
+    elif s.startswith("task_working"):
+        return 3
+    
+def relabel_all(labels):
+    lab  = [relabel(s) for s in labels]
+    return np.array(lab)
