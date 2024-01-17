4cc193ab598e527da526b646703269d1cdf838ec
BNijmeijer <basnijmeijer@live.nl>
2024-01-17 18:07:39 +0100

    z-score scaling over alle data

--- main.py
+++ main.py
@@ -7,49 +7,70 @@ import matplotlib.pyplot  as plt
 import preprocessing as p
 import models as m
 
-#y_train, x_train, y_test, x_test = p.load_intra()
+def main():
+    y_intra_train, x_intra_train, y_intra_test, x_intra_test = p.load_intra()
 
-y_train, x_train = p.load_cross_train()
-y_test, x_test = p.load_cross_test("1") # corresponds to the folders
+    y_cross_train, x_cross_train = p.load_cross_train()
+    y1_cross_test, x1_cross_test = p.load_cross_test("1") # corresponds to the folders
+    y2_cross_test, x2_cross_test = p.load_cross_test("2")
+    y3_cross_test, x3_cross_test = p.load_cross_test("3")
 
-amount = 5
+    amount = 5
 
-# Splits the files into <amount> files and zscales these
-x_train = p.preprocess_multiple(x_train, amount)
-x_test = p.preprocess_multiple(x_test, amount)
+    X_train_intra, y_train_intra,\
+        X_val_intra, y_val_intra,\
+        X_test_intra, y_test_intra,\
+        X_train_cross, y_train_cross,\
+        X_val_cross, y_val_cross,\
+        X1_test_cross, y1_test_cross, \
+        X2_test_cross, y2_test_cross, \
+        X3_test_cross, y3_test_cross = p.preprocess_all(x_intra_train, y_intra_train, \
+                                                        x_intra_test, y_intra_test, \
+                                                        x_cross_train, y_cross_train, \
+                                                        x1_cross_test, y1_cross_test, \
+                                                        x2_cross_test, y2_cross_test, \
+                                                        x3_cross_test, y3_cross_test)
 
-# The repeat is needed to handle the creation of additional files from one file (Het idee van Bas van afgelopen woensdag)
-y_train = np.repeat(p.relabel_all(y_train), amount)
-y_test = np.repeat(p.relabel_all(y_test), amount)
+    # Splits the files into <amount> files and zscales these
+    #x_train = p.preprocess_multiple(x_train, amount)
+    #x_test = p.preprocess_multiple(x_test, amount)
 
-#y_train = np.array([0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3])
-#y_test = np.array([0,0,1,1,2,2,3,3])
+    # The repeat is needed to handle the creation of additional files from one file (Het idee van Bas van afgelopen woensdag)
+    #y_train = np.repeat(p.relabel_all(y_train), amount)
+    #y_test = np.repeat(p.relabel_all(y_test), amount)
 
+    #y_train = np.array([0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3])
+    #y_test = np.array([0,0,1,1,2,2,3,3])
+    print(np.shape(X_train_intra))
+    print(np.shape(y_train_intra))
+    print(np.shape(X_test_intra))
+    print(np.shape(y_test_intra))
 
-
-model = m.rnnmodel()
-model.build()
-#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                metrics=['accuracy'])
-model.summary()
-
-#epochs is maybe een hyperparameter?
-history = model.fit(x_train, y_train, epochs=5, 
-                    validation_data=(x_test, y_test))
-
-
-# Plots the two accuracies against the epochs
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
-plt.show()
+    return
 
 
+    model = m.rnnmodel()
+    model.build()
+    #model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.compile(optimizer='adam',
+                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    metrics=['accuracy'])
+    model.summary()
+
+    #epochs is maybe een hyperparameter?
+    history = model.fit(x_train, y_train, epochs=5, 
+                        validation_data=(x_test, y_test))
 
 
+    # Plots the two accuracies against the epochs
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
--- preprocessing.py
+++ preprocessing.py
@@ -31,24 +31,24 @@ def downsample_matrix(matrix, factor):
     return result_matrix
 
 def load_intra():
-    filenames = os.listdir("Intra/train")
-    x_train = [get_matrix("Intra/train/" + name) for name in filenames]
+    filenames = os.listdir("./data/Intra/train")
+    x_train = [get_matrix("./data/Intra/train/" + name) for name in filenames]
     y_train = ['_'.join(name.split('_')[:-2]) for name in filenames]
 
-    filenames = os.listdir("Intra/test")
-    x_test = [get_matrix("Intra/test/" + name) for name in filenames]
+    filenames = os.listdir("./data/Intra/test")
+    x_test = [get_matrix("./data/Intra/test/" + name) for name in filenames]
     y_test = ['_'.join(name.split('_')[:-2]) for name in filenames]
     return y_train, x_train, y_test, x_test
 
 def load_cross_train():
-    filenames = os.listdir("Cross/train")
-    x_train = [get_matrix("Cross/train/" + name) for name in filenames]
+    filenames = os.listdir("./data/Cross/train")
+    x_train = [get_matrix("./data/Cross/train/" + name) for name in filenames]
     y_train = ['_'.join(name.split('_')[:-2]) for name in filenames]
     return y_train, x_train
 
 def load_cross_test(nr):
-    filenames = os.listdir("Cross/test"+nr)
-    x_train = [get_matrix("Cross/test"+nr+"/" + name) for name in filenames]
+    filenames = os.listdir("./data/Cross/test"+nr)
+    x_train = [get_matrix("./data/Cross/test"+nr+"/" + name) for name in filenames]
     y_train = ['_'.join(name.split('_')[:-2]) for name in filenames]
     return y_train, x_train
 
@@ -89,5 +89,62 @@ def preprocess_multiple(matrices, factor):
         
     zs_mat = [np.transpose(zscore(matrix)) for matrix in mats]
     return np.array(zs_mat)
-    
 
+def zs_scale(mat):
+    return zscore(mat)
+
+
+def downsample(X, y, downsample_factor=8):
+    # creates a larger, downsampled dataset.
+    X_train = []
+    y_train = []
+
+    for (i,data) in enumerate(X):
+        X_train.extend([data[:,j::downsample_factor] for j in range(downsample_factor)])
+        y_train.extend(np.repeat(y[i],8))
+
+    return X_train, y_train
+
+def build_val(X,y):
+    return X[:-1], X[-1], y[:-1], y[-1]
+
+def preprocess_all(train_intra, labels_train_intra, test_intra, labels_test_intra, train_cross, labels_train_cross, test1_cross, labels_test1_cross, test2_cross, labels_test2_cross, test3_cross, labels_test3_cross):
+    # Downsample all data
+    X_train_intra, y_train_intra = downsample(train_intra, labels_train_intra)
+    X_train_cross, y_train_cross = downsample(train_cross, labels_train_cross)
+    X_test_intra, y_test_intra = downsample(test_intra, labels_test_intra)
+    X_test1_cross, y1_test_cross = downsample(test1_cross, labels_test1_cross)
+    X_test2_cross, y2_test_cross = downsample(test2_cross, labels_test2_cross)
+    X_test3_cross, y3_test_cross = downsample(test3_cross, labels_test3_cross)
+
+    # Matrix black magic in order to make z-score scaling work
+    # Order: intra (training, test), cross (training, test1, test2, test3)
+    full_matrix = []
+    delims = []
+    full_matrix.extend(X_train_intra)
+    delims.append(np.shape(X_train_intra)[0])
+    full_matrix.extend(X_test_intra)
+    delims.append(delims[-1] + np.shape(X_test_intra)[0])
+    full_matrix.extend(X_train_cross)
+    delims.append(delims[-1] + np.shape(X_train_cross)[0])
+    full_matrix.extend(X_test1_cross)
+    delims.append(delims[-1] + np.shape(X_test1_cross)[0])
+    full_matrix.extend(X_test2_cross)
+    delims.append(delims[-1] + np.shape(X_test2_cross)[0])
+    full_matrix.extend(X_test3_cross)
+
+    print(np.shape(full_matrix))
+
+    # Perform z-score scaling
+    scaled_data = zs_scale(full_matrix)
+    
+    # Un-black-magicification
+    split_data = np.split(scaled_data, delims, axis=0)
+    assert (split_data[0] == X_train_intra)
+
+    # Create validation datasets
+    X_train_intra, X_val_intra, y_train_intra, y_val_intra = build_val(scaled_data[0], y_train_intra)
+    X_train_cross, X_val_cross, y_train_cross, y_val_cross = build_val(scaled_data[1], y_train_cross)
+
+    return X_train_intra, y_train_intra, X_val_intra, y_val_intra, scaled_data[2], y_test_intra, X_train_cross, y_train_cross, X_val_cross, y_val_cross, scaled_data[3], y1_test_cross, scaled_data[4], y2_test_cross, scaled_data[5], y3_test_cross
+    
